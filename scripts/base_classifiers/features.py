aa02c021a33f6698ac8843e9cb40847c85df95d5
ntran18 <108908370+ntran18@users.noreply.github.com>
2025-02-20 21:17:11 -0800

    Add code to offload model to CPU to save GPU memmory usage

--- scripts/base_classifiers/features.py
+++ scripts/base_classifiers/features.py
@@ -8,6 +8,8 @@ import torch
 import esm
 import pandas as pd
 from decimal import Decimal
+from fairscale.nn.data_parallel import FullyShardedDataParallel as FSDP
+from fairscale.nn.wrap import enable_wrap, wrap
 
 class BaseFeatureExtraction:
     def __init__(self):
@@ -82,46 +84,60 @@ class FeatureExtraction1_3(BaseFeatureExtraction):
         self._load_esm_model(model_name)
 
     def _load_esm_model(self, model_name):
-        self.model, self.alphabet = esm.pretrained.load_model_and_alphabet(model_name)
-        self.batch_converter = self.alphabet.get_batch_converter()
-        self.model.eval()
+        if model_name == "esm2_t48_15B_UR50D":
+            # offload the model to CPU to reduce GPU memory usage. Ex: https://github.com/facebookresearch/esm/blob/main/examples/esm2_infer_fairscale_fsdp_cpu_offloading.py
+
+            # init the distributed world with world_size 1
+            url = "tcp://localhost:23456"
+            torch.distributed.init_process_group(backend="nccl", init_method=url, world_size=1, rank=0)
+            
+            model_data, regression_data = esm.pretrained._download_model_and_regression_data(model_name)
+            
+            # initialize the model with FSDP wrapper
+            fsdp_params = dict(
+                mixed_precision=True,
+                flatten_parameters=True,
+                state_dict_device=torch.device("cpu"),  # reduce GPU mem usage
+                cpu_offload=True,  # enable cpu offloading
+            )
+            
+            with enable_wrap(wrapper_cls=FSDP, **fsdp_params):
+                model, vocab = esm.pretrained.load_model_and_alphabet_core(
+                    model_name, model_data, regression_data
+                )
+                self.batch_converter = vocab.get_batch_converter()
+                model.eval()
+
+                # Wrap each layer in FSDP separately
+                for name, child in model.named_children():
+                    if name == "layers":
+                        for layer_name, layer in child.named_children():
+                            wrapped_layer = wrap(layer)
+                            setattr(child, layer_name, wrapped_layer)
+                self.model = wrap(model)
+        else:
+            self.model, alphabet = esm.pretrained.load_model_and_alphabet(model_name)
+            self.batch_converter = alphabet.get_batch_converter()
+            self.model.eval()
+            self.model.to(self.device)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model.to(self.device)
         self.repr_layers = int(model_name.split("_")[1].replace("t", ""))
     
     def extract_features(self, sequences, neq_values):
-        print("Feature extraction version 1.3 using two GPUs")
+        print("Feature extraction version 1.3")
         
         seq_embedding, targets = [], []
-        num_gpus = torch.cuda.device_count()
-        device0 = torch.device("cuda:0")
-        device1 = torch.device("cuda:1") if num_gpus > 1 else device0  # Use one or two GPUs
-        # Split sequences across GPUs
-        split_idx = len(sequences) // 2
-        seq_batches = [(sequences[:split_idx], neq_values[:split_idx], device0),
-                    (sequences[split_idx:], neq_values[split_idx:], device1)]
-        
-        for seqs, neqs, device in seq_batches:
-            self.model.to(device)  # Move model to the correct GPU
-            print(f"Processing {len(seqs)} sequences on {device}")
-            
-            for i, seq in enumerate(seqs):
-                data = [(f"protein_{i}", seq)]
-                labels, strs, tokens = self.batch_converter(data)
-                
-                with torch.no_grad():
-                    results = self.model(tokens.to(device), repr_layers=[self.repr_layers])
-                    token_embedding = results["representations"][self.repr_layers]
-                    
-                    # Adjust embedding extraction for ESM1 vs ESM2
-                    if self.model_name.startswith("esm1"):
-                        residue_embeddings = token_embedding[0, 1:]
-                    else:
-                        residue_embeddings = token_embedding[0, 1:-1]
-
-                    seq_embedding.extend(residue_embeddings.cpu().numpy())
-                    torch.cuda.empty_cache()
-            for neq in neqs:
-                targets.extend(neq)
-
+        for i, seq in enumerate(sequences):
+            data = [(f"protein_{i}", seq)]
+            labels, strs, tokens = self.batch_converter(data)
+            with torch.no_grad():
+                results = self.model(tokens.to(self.device), repr_layers=[self.repr_layers])
+                token_embedding = results["representations"][self.repr_layers]
+                # If model is ESM 2 then token embedding is 1:-1 for second dimension, else 1: for esm 1
+                if self.model_name.startswith("esm1"):
+                    residue_embeddings = token_embedding[0, 1:]
+                else:
+                    residue_embeddings = token_embedding[0, 1:-1]
+                seq_embedding.extend(residue_embeddings.cpu().numpy())
+                targets.extend(neq_values[i])
         return seq_embedding, targets
