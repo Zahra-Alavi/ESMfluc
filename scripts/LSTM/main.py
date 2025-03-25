4186795e68a9a91378411aca2819be2ebe024163
ntran18 <108908370+ntran18@users.noreply.github.com>
2025-03-24 17:57:22 -0700

    Adding code to try different dropout rates

--- scripts/LSTM/main.py
+++ scripts/LSTM/main.py
@@ -12,13 +12,22 @@ from arguments import parse_arguments
 from train import run_training
 from train2 import train
 
+def dropout_rate_learning(args):
+    dropout_rates = [0.1, 0.2, 0.3, 0.4, 0.5]
+    for dropout in dropout_rates:
+        args.dropout = dropout
+        args.result_dir = "../../results/dropout_{}".format(dropout)
+        train(args)
 def main():
     parser = parse_arguments()
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
-    # run_training(args)
-    train(args)
+    if args.dropout_rate_learning:
+        dropout_rate_learning(args)
+    else:
+        # run_training(args)
+        train(args)
 
 if __name__ == "__main__":
     main()
