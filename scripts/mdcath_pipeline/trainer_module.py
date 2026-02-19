b4c83ecbe0ef26396389da8abf99f17e737637e3
ntran18 <108908370+ntran18@users.noreply.github.com>
2026-02-19 00:45:17 -0800

    Adding more evaluation metrics: mean absolute error, correlation coefficient

--- scripts/mdcath_pipeline/trainer_module.py
+++ scripts/mdcath_pipeline/trainer_module.py
@@ -2,6 +2,7 @@ import torch
 import lightning as L
 from torch.optim import AdamW
 from losses import masked_mse_loss, weighted_masked_mse_loss
+from torchmetrics.regression import MeanAbsoluteError, PearsonCorrCoef, SpearmanCorrCoef
 
 class EsmFlucTrainer(L.LightningModule):
     def __init__(self, model, lr=1e-5, weight_threshold=3.0, weight_factor=5.0, loss_type='weighted', weight_decay=1e-2, masked_value=-100):
@@ -14,6 +15,10 @@ class EsmFlucTrainer(L.LightningModule):
         self.weight_decay = weight_decay
         self.masked_value = masked_value
         self.save_hyperparameters(ignore=['model'])
+        
+        self.val_mae = MeanAbsoluteError()
+        self.val_spearman = SpearmanCorrCoef()
+        self.val_pearson = PearsonCorrCoef()
         
     def training_step(self, batch, batch_idx):
         # Forward pass
@@ -47,6 +52,9 @@ class EsmFlucTrainer(L.LightningModule):
             loss = masked_mse_loss(preds, batch['labels'], self.masked_value)
         
         self.log("val_loss", loss, prog_bar=True)
+        self.log("val_mae", self.val_mae, prog_bar=True, on_epoch=True)
+        self.log("val_spearman", self.val_spearman, prog_bar=True, on_epoch=True)
+        self.log("val_pearson", self.val_pearson, prog_bar=True, on_epoch=True)
         return loss
     
     def configure_optimizers(self):
